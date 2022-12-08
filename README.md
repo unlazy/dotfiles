ce654384ef695885901f2843b4ab0ff0330449f7
Maksim Chayka <unlazy@gmail.com>
2022-12-09 00:50:55 +0700

    Added initial config for dotfiles

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# dotfiles
+# github.com/unlazy/dotfiles
+
+dotfiles, managed with [`chezmoi`](https://github.com/twpayne/chezmoi).
+
+## Install
+
+This'll install Homebrew and chezmoi. After installation `chezmoi` will be initialized.
+
+```shell
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/unlazy/dotfiles/master/install.sh)"
+```
+
+## Firefox styling
+
+TODO.
---
+++ dot_zplug.sh
@@ -0,0 +1,28 @@
+[ -d /usr/local/opt/zplug ] && export ZPLUG_HOME=/usr/local/opt/zplug
+[ -d /opt/homebrew/opt/zplug ] && export ZPLUG_HOME=/opt/homebrew/opt/zplug
+
+source $ZPLUG_HOME/init.zsh
+
+# zplug "plugins/nvm", from:oh-my-zsh
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/autojump", from:oh-my-zsh
+
+# zplug "bigH/git-fuzzy", as:command, use:"bin/git-fuzzy"
+
+# zplug "junegunn/fzf", use:"shell/*.zsh", defer:2
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "zsh-users/zsh-autosuggestions", as:plugin, defer:2
+zplug 'zsh-users/zsh-completions', depth:1 # more completions
+
+zplug "mafredri/zsh-async", from:github
+zplug "sindresorhus/pure", use:pure.zsh, from:github, at:main, as:theme
+
+zplug load
+
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
